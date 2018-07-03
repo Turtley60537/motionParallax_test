064bfbd7a8b72309bf053ea03c7cc6297ca7d799
Kame <ev60537@meiji.ac.jp>
2018-07-03 23:29:37 +0900

    start video add frontalface.js

--- sample.js
+++ sample.js
@@ -11,12 +11,15 @@ const coords = detector.detect(video, 1);
 
 let localStream;
 
+
+
 // start local video
 function startVideo() {
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
     .then(function (stream) { // success
     localStream = stream;
     video.src = window.URL.createObjectURL(localStream);
+
     }).catch(function (error) { // error
     console.error('mediaDevice.getUserMedia() error:', error);
     return;
